e5654ad1fc17da2f4e7fcfb6e2759bdb5eb424a2
Ábner <abnerprincipe@outlook.com>
2025-12-18 11:32:10 -0300

    Uptaded 1.1 #php
    Criação das páginas de php para as motos compradas e alugadas. As páginas index e contato foram alteradas. motos.css adicionada.

---
+++ php/motos_comprar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rei das Motos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style/motos.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="img/a.png" type="image/x-icon">
+</head>
+<header>
+    <nav>
+        <img src="img/image.png" alt="" id="logo">
+         <ul class="menu">
+            <li><a href="index.html">Home</a></li>
+            <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="motos.html" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Motos
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/php/motos_comprar.php">À vendas</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/php/motos_alugadas.php">Alugadas</a></li>
+          </ul>
+        </li>
+            <li><a href="contato.html">Contato</a></li>
+         </ul>
+    </nav>
+</header>
+<body>
+
+</body>
+</html>
